ee28ba279c40eee56ac5365b58f787517b167b1c
AG67176 <AG67176@POOLW42SLPC6827.ind.zensar.com>
2022-07-03 19:18:55 +0530

    Added sop

--- Thymeleaf_Project/src/main/java/com/zensar/ThymeleafProjectApplication.java
+++ Thymeleaf_Project/src/main/java/com/zensar/ThymeleafProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThymeleafProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hi");
 		SpringApplication.run(ThymeleafProjectApplication.class, args);
 	}
 
