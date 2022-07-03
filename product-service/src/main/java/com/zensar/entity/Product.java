de480773629f857c6cb8afb31bdb71b0293168b7
AG67176 <AG67176@POOLW42SLPC6827.ind.zensar.com>
2022-07-03 19:15:23 +0530

    thymeleaf project

--- product-service/src/main/java/com/zensar/entity/Product.java
+++ product-service/src/main/java/com/zensar/entity/Product.java
@@ -19,7 +19,66 @@ public class Product {
 	private String productName;
 	private String discription;
 	private Double price;
-	
-	
+	@Transient
+	private String couponCode;
+
+	public Product() {
+		super();
+	}
+
+	public Product(int productId, String productName, String discription, Double price, String couponCode) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.discription = discription;
+		this.price = price;
+		this.couponCode = couponCode;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDiscription() {
+		return discription;
+	}
+
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getCouponCode() {
+		return couponCode;
+	}
+
+	public void setCouponCode(String couponCode) {
+		this.couponCode = couponCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", discription=" + discription
+				+ ", price=" + price + ", couponCode=" + couponCode + "]";
+	}
 
 }
